dc860816ab526c6ce47d621d3878126341bddd71
Max12 <92970133+enterdone@users.noreply.github.com>
2023-06-10 16:55:06 +0400

    10.06

--- pg.js
+++ pg.js
@@ -8,13 +8,13 @@ const pool = new Pool({
 	database: "ethxqheb",
 	password: 'N0L5F1Z0guYnyItKNPtRk1Kjr7snGCmi',
 	port: 5432,
-	max:5,
+	max: 5,
 	connectionTimeoutMillis: 10000
 });
 // init_db(pool);  //create db mytable in postgres
 // pool.query('SELECT * FROM mytable')
 
-function save_message_bd(user_id,message_id) {
+function save_message_bd(user_id, message_id) {
 	pool.query('INSERT INTO mytable (user_id, message_id,date, day_interval) VALUES ($1, $2, now()+INTERVAL \'1 day\', 1.1)', [user_id, message_id], (err, res) => {
 		if (err) {
 			console.log(err.stack);
@@ -22,84 +22,79 @@ function save_message_bd(user_id,message_id) {
 			// console.log('Row inserted with ID:', res.rows[0].id);
 		}
 	});
-	
+
 }
 
-async function query_get_message (user_id){
-	const query  = `WITH updated_row AS (
+async function query_get_message(user_id) {
+	const query = `WITH updated_row AS (
 		UPDATE mytable
 		SET date = date + (day_interval * INTERVAL '1 day'),
 			day_interval = CASE WHEN day_interval <= 300 THEN day_interval * 1.42 ELSE day_interval END
 		WHERE message_id = (SELECT message_id FROM mytable WHERE user_id = ${user_id}::text ORDER BY date LIMIT 1)
 		RETURNING *
 	) SELECT * FROM updated_row;;`
-try{
-	 const result = await pool.query(query);
+	try {
+		const result = await pool.query(query);
 
-    const rows =  result.rows;
-	console.log('pg_rows______________');
-	
-	 console.dir(rows.rows)
-	 return rows
-	}catch(e){ console.log("🤷‍♂️err query_get_message",e)}
+		const rows = result.rows;
+		console.log('pg_rows______________');
+
+		console.dir(rows.rows)
+		return rows
+	} catch (e) { console.log("🤷‍♂️err query_get_message", e) }
 
 }
 
 
-const todayJob =  new Promise((resolve, reject) => {
-    // const query = `  WITH updated_rows AS (
-    //     UPDATE mytable
-    //     SET date = date + (day_interval || ' day')::interval,
-    //      day_interval = day_interval * 1.42
-    //     WHERE date <= CURRENT_DATE   
-    //     RETURNING *
-    //   ) 
-      
+const todayJob = new Promise((resolve, reject) => {
+	// const query = `  WITH updated_rows AS (
+	//     UPDATE mytable
+	//     SET date = date + (day_interval || ' day')::interval,
+	//      day_interval = day_interval * 1.42
+	//     WHERE date <= CURRENT_DATE   
+	//     RETURNING *
+	//   ) 
+
 	console.log('todayJob =  new Promise');
-	
+
 	//   SELECT user_id, json_agg(updated_rows) as objects
 	//   FROM updated_rows
 	//   GROUP BY user_id;
-    //                      `
+	//                      `
 	const query = `  WITH updated_rows AS (
         UPDATE mytable
         SET date = date + (day_interval || ' day')::interval,
-         day_interval = day_interval * 1.42
+         day_interval = CASE WHEN day_interval <= 300 THEN day_interval * 1.42 ELSE day_interval END
         WHERE date <= CURRENT_DATE   
         RETURNING *
       ) 
 	  SELECT user_id, json_agg(updated_rows) as objects
 	  FROM updated_rows
 	  GROUP BY user_id;
-                         ` 
+                         `
 	pool.query(query)
-      .then(result => {
-        const rows = result.rows;
-        resolve(rows);
-      })
-
-	  	// .then(pool.query(`DROP TABLE IF EXISTS tt`)
+		.then(result => {
+			const rows = result.rows;
+			resolve(rows);
+		})
+		// .then(pool.query(`DROP TABLE IF EXISTS tt`)
 		// .then(()=>pool.query(`CREATE TABLE tt AS SELECT * FROM mytable`)))
-		 
-	  	 
+		.catch(error => { reject(error); });
+});
 
-      .catch(error => {reject(error);});
-  });
- 
 
-  function delete_from_BD(userId, messageId){
+function delete_from_BD(userId, messageId) {
 	pool.query(`DELETE FROM mytable
 	WHERE message_id = ${messageId}::integer AND user_id = ${userId}::text;
 	`)
-	.then(console.log('deleted id user',userId, messageId ))
-	.catch(e=>console.log(e))
-  }
+		.then(console.log('deleted id user', userId, messageId))
+		.catch(e => console.log(e))
+}
 
 // async function insertData(...values)
 module.exports = {
 	save_message_bd,
 	query_get_message,
-	todayJob,delete_from_BD
+	todayJob, delete_from_BD
 }
 
- 
--- time.js
+++ time.js
@@ -1,34 +1,36 @@
 const schedule = require('node-schedule');
-const {todayJob}=require('./pg.js');
+const { todayJob } = require('./pg.js');
 
 //TODO => when bot start & id settings changed SQL
- const timeDb = {
-     "2252839": { dayDistance: 12 },
-     "424244": { dayDistance: 8 }
-     ,1293060843 : { dayDistance: 1 , time: 5}
-   };
+const timeDb = {
+    "2252839": { dayDistance: 12 },
+    "424244": { dayDistance: 8 }
+    , 1293060843: { dayDistance: 3, startHourTheDay: 17 },
+    472758383: {dayDistance: 10, startHourTheDay: 18}
+};
 
-const timer_start = (bot,f) =>{
-    const job = schedule.scheduleJob('0 0 0 * * *', () => { 
-        server_notification = `База полученна
-        Текущее время сервера:
-         ${new Date()}`  
-            
-    console.log(server_notification);
+const timer_start = (bot, f) => {
+    const job = schedule.scheduleJob('0 0 0 * * *', () => {
+        
+        console.log('time.js timer_start');
+        server_notification = 'База полученна \n  Текущее время сервера:\n' + new Date()
+        console.log(server_notification);
 
-    bot.telegram.sendMessage(472758383,server_notification ) ;
-    console.log('time.js timer_start');
-    timer_planning(bot,f);
-                                });
- timer_planning(bot,f)    }// 1 time when servers start (test)
-     
+        bot.telegram.sendMessage(472758383, server_notification);
+
+        timer_planning(bot, f);
+    });
+    timer_planning(bot, f)
+}// 1 time when servers start (test)
 
 
-const timer_planning = (bot,f) => {
+
+const timer_planning = (bot, f) => {
     console.log("timer_planning")
-    todayJob.then(rows => rows.map(row =>
-        setJobForUser(row.user_id, row.objects ,bot ,f )
-        )).catch(error => {console.error(error)});
+    todayJob.then(rows => {
+        console.dir(rows[0])
+        rows.map(row =>setJobForUser(row.user_id, row.objects, bot, f)
+    )}).catch(error => { console.error(error) });
 }
 
 
@@ -37,47 +39,48 @@ const timer_planning = (bot,f) => {
 
 const setJobForUser = (user_id, messages, bot, f) => {
     console.log('setJobForUser');
-    
-    let cronTime,hours,minutes, msgTime,date    
+
+    let cronTime, hours, minutes, msgTime, date
     console.log('user_id:', user_id);
-    
+
     // const dayDistanceDemicalHours = timeDb[user_id] && timeDb[user_id].dayDistance ? timeDb[user_id].dayDistance : 12;
 
-   const dayDistanceDemicalHours = timeDb[user_id]?.dayDistance ||    12
-//    const dayDistanceDemicalHours =     12
-   console.log(dayDistanceDemicalHours);
-
-   const  startHourTheDay =   11
-   console.log(dayDistanceDemicalHours, startHourTheDay)
-   const utc = 4
-   const timeZone = - utc + startHourTheDay   
-
-   step = dayDistanceDemicalHours/messages.length
-   date = new Date()
-   for(let i=0,j=0; j<dayDistanceDemicalHours; i++,j+=step ){
-
-    msgTime = (j).toFixed(2)
-    hours = Math.floor(msgTime) 
-    demicalMin = msgTime - hours
-    minutes = Math.round(demicalMin*60)
-    date.setUTCHours(hours+timeZone ); // Установка часов (14 + 4 = 18)
-    console.log('hours',hours,'timeZone',timeZone,'demicalMin',demicalMin,'demicalMin',demicalMin);
+    const dayDistanceDemicalHours = timeDb[user_id]?.dayDistance || 12
+    //    const dayDistanceDemicalHours =     12
+    console.log(dayDistanceDemicalHours);
     
-date.setUTCMinutes(minutes ); // Установка минут
-console.log(date,i,"i", j, "j", step,"step",msgTime, "msgTime" )
+    const startHourTheDay = timeDb[user_id]?.startHourTheDay || 9
+    console.log(dayDistanceDemicalHours, startHourTheDay)
+    const utc = 4
+    const timeZone = - utc + startHourTheDay
+
+    step = dayDistanceDemicalHours / messages.length
+    date = new Date()
+    for (let i = 0, j = 0; j < dayDistanceDemicalHours; i++, j += step) {
+
+        msgTime = (j).toFixed(2)
+        hours = Math.floor(msgTime)
+        demicalMin = msgTime - hours
+        minutes = Math.round(demicalMin * 60)
+        date.setUTCHours(hours + timeZone); // Установка часов (14 + 4 = 18)
+        console.log('hours', hours, 'timeZone', timeZone, 'demicalMin', demicalMin, 'demicalMin', demicalMin);
+
+        date.setUTCMinutes(minutes); // Установка минут
+        console.log(date, i, "i", j, "j", step, "step", msgTime, "msgTime")
 
 
-schedule.scheduleJob(date, () => {
-    console.log(`Running ${minutes}`,hours,date);
-    console.log(messages[i])
-    try{
-        f(bot, messages[i].user_id,messages[i].message_id)
-    }catch(err){console.log(err);}
-    // Ваш код для выполнения задания
-  });
-  
-}}
+        schedule.scheduleJob(date, () => {
+            console.log(`Running ${minutes}`, hours, date);
+            console.log(messages[i])
+            try {
+                f(bot, messages[i].user_id, messages[i].message_id)
+            } catch (err) { console.log(err); }
+            // Ваш код для выполнения задания
+        });
+
+    }
+}
 
 
- 
-module.exports = {timer_start, }
+
+module.exports = { timer_start, }
